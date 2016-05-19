1f302bffedec56b4fe18db5eb1140b1a9f1259c7
orazdow <ollierazdow@hotmail.com>
2016-05-19 19:22:37 -0400

    yun / arm condition

--- TrueNum.h
+++ TrueNum.h
@@ -20,7 +20,7 @@ void setVal(const char* intoken, float invalue);
 boolean getReturnBool();
 float getVal(const char* intoken);
 
-private:
+//private:
 
 #ifdef __AVR__
 static const uint8_t num = 4; //makes big mem diff... 
--- TrueNumClient.ino
+++ TrueNumClient.ino
@@ -1,3 +1,5 @@
+#ifndef __AVR__
+
 #include "TrueNum.h"
 #include "TestSensor.h"
 #include <SPI.h>
@@ -87,4 +89,53 @@ void printWifiStatus() {
   Serial.print(rssi);
   Serial.println(" dBm");
 }
+
+#else
+
+#include "TrueNum.h"
+#include "TestSensor.h"
+#include <Bridge.h>
+#include <YunClient.h>
+
+//TrueNum myNum("test@adomain.abc", "condor", "public-iot", "arduino1");
+TrueNum myNum("ollie@truenum.com", "Randal9?", "IoT-Project", "ard123");
+
+YunClient client;
+TestSensor mySensor;
+
+void setup() {
+  
+Bridge.begin();
+Serial.begin(9600);
+while(!Serial); 
+//sketch will start when serial monitor is opened
+
+//myNum.getQuery(client);
+
+}
+
+void loop() {
+  
+//myNum.setVal("$sensor-temp", mySensor.getTemperature());
+//myNum.setVal("$sensor-hum", mySensor.getHumidity() ); 
+
+myNum.setVal("$DHT-temp", mySensor.getTemperature());
+myNum.setVal("$thing1f", mySensor.getHumidity());
+
+Serial.println(myNum.getVal("$DHT-temp"));
+
+myNum.getQuery(client);
+myNum.makeCall(client);
+
+//myNum.makeCall( "temperature of myArduino = $DHT-temp < 70 F", client );
+
+if(myNum.getReturnBool()){
+  digitalWrite(13,HIGH);
+} else { digitalWrite(13,LOW);}
+
+}
+
+#endif
+
+
 
